163f796499a770818447ef1f57bf6123a478f571
Valeria Vicious <59263802+ValeriaVicious@users.noreply.github.com>
2021-08-30 10:07:40 +0200

    описание контейнера

--- Snake-MVVM/Assets/Code/Models/Interfaces/IContainerLocation.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-namespace SnakeTheClassicGameOnMVVM
-{
-    public interface IContainerLocation
-    {
-
-    }
-}
---
+++ Snake-MVVM/Assets/Code/Models/ViewModel/Interfaces/ILocationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace SnakeTheClassicGameOnMVVM
+{
+    public interface ILocationViewModel
+    {
+        public ILocationModel LocationModel { get; }
+        public void MoveTheSnake(float x, float y);
+        public event Action<ILocationChangeModel> OnLocationChangeEvent;
+    }
+}
