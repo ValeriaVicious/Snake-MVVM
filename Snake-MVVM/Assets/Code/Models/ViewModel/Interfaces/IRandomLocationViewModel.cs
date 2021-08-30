f2e96d2dde75591a052e3a26d6d471504da999cc
Valeria Vicious <59263802+ValeriaVicious@users.noreply.github.com>
2021-08-30 11:08:28 +0200

    описан класс рандома

---
+++ Snake-MVVM/Assets/Code/Models/ViewModel/Interfaces/IRandomLocationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace SnakeTheClassicGameOnMVVM
+{
+    internal interface IRandomLocationViewModel
+    {
+        public ILocationModel LocationModel { get; }
+        public void ChangeTheLocation();
+        public event Action<ILocationChangeModel> OnLocationChangeEvent;
+    }
+}
---
+++ Snake-MVVM/Assets/Code/Models/ViewModel/Model/RandomLocationViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace SnakeTheClassicGameOnMVVM
+{
+    internal sealed class RandomLocationViewModel : IRandomLocationViewModel
+    {
+        #region Field
+
+        private readonly ILocationCoordinates _locationCoordinates;
+        public event Action<ILocationChangeModel> OnLocationChangeEvent;
+
+        #endregion
+
+
+        #region Properties
+
+        public ILocationModel LocationModel { get; set; }
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public RandomLocationViewModel(ILocationModel locationModel,
+            ILocationCoordinates locationCoordinates)
+        {
+            LocationModel = locationModel;
+            _locationCoordinates = locationCoordinates;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void ChangeTheLocation()
+        {
+            var previousX = LocationModel.X;
+            var previousY = LocationModel.Y;
+
+            LocationModel.X = UnityEngine.Random.Range(_locationCoordinates.MinX,
+                _locationCoordinates.MaxX);
+            LocationModel.Y = UnityEngine.Random.Range(_locationCoordinates.MinY,
+                _locationCoordinates.MaxY);
+            var locationChangeModel = new LocationChangeModel(previousX, previousY,
+                LocationModel.X, LocationModel.Y);
+            OnLocationChangeEvent?.Invoke(locationChangeModel);
+        }
+
+        #endregion
+    }
+}
