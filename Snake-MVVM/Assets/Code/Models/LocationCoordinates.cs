29480cda23595dc5e8e802595ccb0a4702b44e53
Valeria Vicious <59263802+ValeriaVicious@users.noreply.github.com>
2021-08-30 09:26:22 +0200

    описание класса координат

---
+++ Snake-MVVM/Assets/Code/Models/LocationCoordinates.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace SnakeTheClassicGameOnMVVM
+{
+    internal sealed class LocationCoordinates : ILocationCoordinates
+    {
+        #region Properties
+        public float MinX { get; set; }
+        public float MaxX { get; set; }
+        public float MinY { get; set; }
+        public float MaxY { get; set; }
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public LocationCoordinates(float minX, float maxX, float minY, float maxY)
+        {
+            MinX = minX;
+            MaxX = maxX;
+            MinY = minY;
+            MaxY = maxY;
+        }
+
+        #endregion
+    }
+}
---
+++ Snake-MVVM/Assets/Code/Models/LocationModel.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace SnakeTheClassicGameOnMVVM
+{
+    internal sealed class LocationModel : ILocationModel
+    {
+        #region Properties
+
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public LocationModel(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        #endregion
+    }
+}
