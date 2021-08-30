4105c85b25495f21452f2bd078d8c5cda6f5b184
Valeria Vicious <59263802+ValeriaVicious@users.noreply.github.com>
2021-08-30 14:30:40 +0200

    описан класс MoveTheSnakeViewModel

---
+++ Snake-MVVM/Assets/Code/Models/ViewModel/Model/MoveTheSnakeViewModel.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace SnakeTheClassicGameOnMVVM
+{
+    internal sealed class MoveTheSnakeViewModel
+    {
+        #region Fields
+
+        private readonly ILocationViewModel _headOfSnakeLocation;
+        private readonly IContainerLocation _snakeContainer;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public MoveTheSnakeViewModel(ILocationViewModel headOfSnakeLocation,
+            IContainerLocation snakeContainer)
+        {
+            _headOfSnakeLocation = headOfSnakeLocation;
+            _snakeContainer = snakeContainer;
+
+            _headOfSnakeLocation.OnLocationChangeEvent += OnHeadChangeLocation;
+        }
+
+        ~MoveTheSnakeViewModel()
+        {
+            _headOfSnakeLocation.OnLocationChangeEvent -= OnHeadChangeLocation;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private void OnHeadChangeLocation(ILocationChangeModel headOfSnakeLocation)
+        {
+            var previousX = headOfSnakeLocation.PreviousCoordinateX;
+            var previousY = headOfSnakeLocation.PreviousCoordinateY;
+
+            foreach (var segment in _snakeContainer)
+            {
+                if (segment != _snakeContainer.First())
+                {
+                    var previousBufferX = segment.LocationModel.X;
+                    var previousBufferY = segment.LocationModel.Y;
+                    segment.MoveTheSnake(previousX, previousBufferY);
+                    previousX = previousBufferX;
+                    previousY = previousBufferY;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
