e6d3de0c4e03c5c208772b733d0d56d89d368bb8
Valeria Vicious <59263802+ValeriaVicious@users.noreply.github.com>
2021-08-30 09:37:16 +0200

    реализация класса, который хранит в себе прежние и текущие координаты

---
+++ Snake-MVVM/Assets/Code/Models/Interfaces/IContainerLocation.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace SnakeTheClassicGameOnMVVM
+{
+    public interface IContainerLocation
+    {
+
+    }
+}
---
+++ Snake-MVVM/Assets/Code/Models/LocationChangeModel.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace SnakeTheClassicGameOnMVVM
+{
+    internal sealed class LocationChangeModel : ILocationChangeModel
+    {
+        #region Fields
+
+        public float PreviousCoordinateY { get; }
+
+        public float PreviousCoordinateX { get; }
+
+        public float CurrentCoordinateY { get; }
+
+        public float CurrentCoordinateX { get; }
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public LocationChangeModel(float previousY, float previousX, float currentY, float currentX)
+        {
+            PreviousCoordinateY = previousY;
+            PreviousCoordinateX = previousX;
+            CurrentCoordinateY = currentY;
+            CurrentCoordinateX = currentX;
+        }
+
+        #endregion
+    }
+}
